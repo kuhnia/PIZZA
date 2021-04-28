1daaa3177ee0ed03376e6d7a4ca87b6c3f9de841
Микола Чепіль <colyachepil@gmail.com>
2021-04-28 07:59:16 +0300

    bisic clasaes

---
+++ PIZZA/AppContent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIZZA
+{
+    public class AppContent : DbContext
+    {
+        public AppContent(DbContextOptions<AppContent> options) : base(options) { }
+
+        public DbSet<>
+    }
+}
