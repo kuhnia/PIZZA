a21addb3b9ea975a08bb658ca0bfdf6f2a0e4c87
Микола Чепіль <colyachepil@gmail.com>
2021-05-14 21:01:31 +0300

    f1

---
+++ dbo.Pizza.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Pizza] ON
+INSERT INTO [dbo].[Pizza] ([Id], [Name], [Category], [Weigth], [Price]) VALUES (NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Pizza] OFF
